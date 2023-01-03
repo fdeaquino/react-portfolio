59ecd3be129c5088e4409d3852955865c7e83ae7
FDeaquino <fidel.deaquino@gmail.com>
2023-01-03 14:47:18 -0600

    created ContactForm component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
+
+  const [currentComponent, setCurrentComponent] = useState('About');
+
   return (
     <div>
       <Nav></Nav>
       <main>
         <Portfolio></Portfolio>
         <About></About>
+        <ContactForm></ContactForm>
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import logo from "../../assets/small/logo.png";
 
 
 function Nav() {
-    const projects = [
-        {
-            name: "Byte Farmer's Market",
-            description:
-                "A digital marketplace for local businesses to launch their store and display their products. Users can create a store profile with associated products, as well as vote on their favorite products.",
-        },
-        {
-            name: "Tempo Run", description: "A search engine that suggests songs with a certain Beat Per Minute (BPM).",
-        },
-        {
-            name: "Taskmaster Pro", description: "A Progressive Web App that allows users to organize their workflow, Kanban style."
-        },
-        {
-            name: "Weather Dashboard",
-            description: "An application that allows users to search the weather for a given city."
-        },
-        {
-            name: "Pizza Hunt",
-            description: "A social media recipe app where users can post creative spins on pizza, and connect with each other based on their love of pizza."
-        }
-    ];
+
+    function colorChange() {
+        console.log("Nav Color Changed")
+    }
+
+    const [currentComponent, setCurrentComponent] = useState('About');
 
 
     return (
@@ -36,7 +21,12 @@ function Nav() {
             <nav>
                 <ul className='flex-row'>
                     <li className='mx-2'>
-                        <a href='#about'>About Me</a>
+                        {currentComponent === 'About' ? (
+                            <a style={{color:'red'}} href='#about'>About</a>
+                        ) : (
+                            <a style={{color:'green'}} href='#about' onClick={colorChange}>About</a>
+                        )}
+                        
                     </li>
                     <li className='mx-2'>
                         <a href='#portfolio'>Portfolio</a>
@@ -52,5 +42,27 @@ function Nav() {
         </header>
     );
 }
+
+// const projects = [
+//     {
+//         name: "Byte Farmer's Market",
+//         description:
+//             "A digital marketplace for local businesses to launch their store and display their products. Users can create a store profile with associated products, as well as vote on their favorite products.",
+//     },
+//     {
+//         name: "Tempo Run", description: "A search engine that suggests songs with a certain Beat Per Minute (BPM).",
+//     },
+//     {
+//         name: "Taskmaster Pro", description: "A Progressive Web App that allows users to organize their workflow, Kanban style."
+//     },
+//     {
+//         name: "Weather Dashboard",
+//         description: "An application that allows users to search the weather for a given city."
+//     },
+//     {
+//         name: "Pizza Hunt",
+//         description: "A social media recipe app where users can post creative spins on pizza, and connect with each other based on their love of pizza."
+//     }
+// ];
 
 export default Nav;
