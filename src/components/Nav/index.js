a2781f85ab6e1d5fdfdef72150e16594046134cd
FDeaquino <fidel.deaquino@gmail.com>
2023-01-19 18:55:59 -0600

    bio edit, nav edit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -15,7 +15,7 @@ function Nav(props) {
     return (
         <header>
             <nav class="navbar navbar-expand-lg navbar-light bsNav">
-                <a class="navbar-brand aBrandStyles" href="#">Fidel Deaquino</a>
+                <h1 class="navbar-brand aBrandStyles" href="#about">Fidel Deaquino</h1>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
